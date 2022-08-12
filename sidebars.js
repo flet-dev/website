032dd982a866b6dc8dc0fc4171ece6a5f2f5f164
Feodor Fitsner <feodor@appveyor.com>
2022-08-11 20:54:36 -0700

    Text.semantics_label

--- sidebars.js
+++ sidebars.js
@@ -172,6 +172,7 @@ module.exports = {
             'controls/shadermask',
             'controls/draggable',
             'controls/dragtarget',
+            'controls/semantics',
           ]
         },
       ]
