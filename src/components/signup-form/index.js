01e4bb39e20271eabcd16130d27b9c095cc103b2
Alan3344 <cleverfairy23@gmail.com>
2023-09-13 23:29:52 +0700

    New and changed instructions
    1. Add automatic translation file structure generation
    2. Modify the fixed string structure of src/pages to Translate
    3. Modify the fixed string structure of src/components/signup-form to Translate
    4. Add Chinese README.zh-CN.md
    5. Modify the code in the document to include function highlighting values
    6. Temporarily delete my own local preprocessing script (in order to merge the main branch)

--- src/components/signup-form/index.js
+++ src/components/signup-form/index.js
@@ -1,77 +1,118 @@
-import React, { useEffect, useRef, useState } from "react";
-import BrowserOnly from '@docusaurus/BrowserOnly';
-import HCaptcha from "@hcaptcha/react-hcaptcha";
-import styles from './styles.module.css';
+import React, { useEffect, useRef, useState } from 'react'
+import BrowserOnly from '@docusaurus/BrowserOnly'
+import HCaptcha from '@hcaptcha/react-hcaptcha'
+import styles from './styles.module.css'
+import Translate, { translate } from '@docusaurus/Translate'
 
 export default function SignupForm() {
-    const [token, setToken] = useState(null);
-    const [email, setEmail] = useState("");
-    const captchaRef = useRef(null);
+  const [token, setToken] = useState(null)
+  const [email, setEmail] = useState('')
+  const captchaRef = useRef(null)
 
-    const onSubmit = (event) => {
-        event.preventDefault();
-        captchaRef.current.execute();
-    };
+  const onSubmit = event => {
+    event.preventDefault()
+    captchaRef.current.execute()
+  }
 
-    const onExpire = () => {
-        console.log("hCaptcha Token Expired");
-    };
+  const onExpire = () => {
+    console.log('hCaptcha Token Expired')
+  }
 
-    const onError = (err) => {
-        console.log(`hCaptcha Error: ${err}`);
-    };
+  const onError = err => {
+    console.log(`hCaptcha Error: ${err}`)
+  }
 
-    useEffect(async () => {
-        if (token) {
-            var data = {
-                email: email,
-                captchaToken: token
-            };
+  useEffect(async () => {
+    if (token) {
+      var data = {
+        email: email,
+        captchaToken: token,
+      }
 
-            // send message
-            const response = await fetch("/api/email-signup", {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(data),
-            });
-            const results = await response.json();
-            console.log(`Results:`, results);
-        }
-    }, [token, email]);
+      // send message
+      const response = await fetch('/api/email-signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      })
+      const results = await response.json()
+      console.log(`Results:`, results)
+    }
+  }, [token, email])
 
-    return (
-        <div id="signup" className={styles.signupForm}>
-            <BrowserOnly fallback={<div>Loading...</div>}>
-                {() => {
-                    if (token) {
-                        // signup submitted
-                        return <div>Thank you! You will receive the confirmation email shortly.</div>
-                    } else if (window.location.href.endsWith('?signup-confirmed')) {
-                        // signup confirmed
-                        return <div><span style={{ fontSize: '25px', marginRight: '10px' }}>🎉</span>Congratulations! You have successfully subscribed to Flet newsletter.</div>
-                    } else {
-                        // signup form
-                        return <form onSubmit={onSubmit}>
-                            <h3>Subscribe to Flet newsletter for project updates and tutorials!</h3>
-                            <input
-                                type="email"
-                                value={email}
-                                placeholder="Your email address"
-                                onChange={(evt) => setEmail(evt.target.value)}
-                            />
-                            <input type="submit" value="Submit" />
-                            <HCaptcha
-                                sitekey="db49a301-288d-491b-9746-ebd3354dc5ff"
-                                size="invisible"
-                                onVerify={setToken}
-                                onError={onError}
-                                onExpire={onExpire}
-                                ref={captchaRef}
-                            />
-                        </form>
-                    }
-                }}
-            </BrowserOnly>
-        </div>
-    );
+  return (
+    <div id='signup' className={styles.signupForm}>
+      <BrowserOnly fallback={<div>Loading...</div>}>
+        {() => {
+          if (token) {
+            // signup submitted
+            return (
+              <div>
+                <Translate
+                  id='indexPage.text.sendEmailTip'
+                  description='Send email notification description'>
+                  Thank you! You will receive the confirmation email shortly.
+                </Translate>
+              </div>
+            )
+          } else if (window.location.href.endsWith('?signup-confirmed')) {
+            // signup confirmed
+            return (
+              <div>
+                <span style={{ fontSize: '25px', marginRight: '10px' }}>
+                  🎉
+                </span>
+                <Translate
+                  id='indexPage.text.subscribedTip'
+                  description='Subscription success notification description'>
+                  Congratulations! You have successfully subscribed to Flet
+                  newsletter.
+                </Translate>
+              </div>
+            )
+          } else {
+            // signup form
+            return (
+              <form onSubmit={onSubmit}>
+                <h3>
+                  <Translate
+                    id='indexPage.text.subscribedSuccessTip'
+                    description='Get update notification success description'>
+                    Subscribe to Flet newsletter for project updates and
+                    tutorials!
+                  </Translate>
+                </h3>
+                <input
+                  type='email'
+                  value={email}
+                  placeholder={translate({
+                    id: 'indexPage.input.emailPlaceholder',
+                    message: 'Your email address',
+                    description: 'Email placeholder text description',
+                  })}
+                  onChange={evt => setEmail(evt.target.value)}
+                />
+                <input
+                  type='submit'
+                  value={translate({
+                    id: 'indexPage.input.submit',
+                    message: 'Submit',
+                    description: 'Submit button text description',
+                  })}
+                />
+                <HCaptcha
+                  sitekey='db49a301-288d-491b-9746-ebd3354dc5ff'
+                  size='invisible'
+                  onVerify={setToken}
+                  onError={onError}
+                  onExpire={onExpire}
+                  ref={captchaRef}
+                />
+              </form>
+            )
+          }
+        }}
+      </BrowserOnly>
+    </div>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,127 +1,268 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
-import Tabs from '@theme/Tabs';
-import TabItem from '@theme/TabItem';
+import React from 'react'
+import clsx from 'clsx'
+import Layout from '@theme/Layout'
+import Link from '@docusaurus/Link'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import useBaseUrl from '@docusaurus/useBaseUrl'
+import styles from './styles.module.css'
+import Tabs from '@theme/Tabs'
+import TabItem from '@theme/TabItem'
 import SignupForm from '@site/src/components/signup-form'
-import CodeBlock from '@theme/CodeBlock';
+import CodeBlock from '@theme/CodeBlock'
+import Translate, { translate } from '@docusaurus/Translate'
 
 const features = [
   {
-    title: <>From idea to app in minutes</>,
+    title: (
+      <>
+        <Translate
+          id='indexPage.features.label1.title'
+          description='features label1 title'>
+          From idea to app in minutes
+        </Translate>
+      </>
+    ),
     imageUrl: 'img/pages/home/feature-bolt.svg',
     description: (
       <>
-        An internal tool or a dashboard for your team, weekend project, data entry form, kiosk app or high-fidelity prototype
-        - Flet is an ideal framework to quickly hack a great-looking interactive apps to serve a group of users.
+        <Translate
+          id='indexPage.features.label1.content'
+          description='features label1 content'>
+          An internal tool or a dashboard for your team, weekend project, data
+          entry form, kiosk app or high-fidelity prototype - Flet is an ideal
+          framework to quickly hack a great-looking interactive apps to serve a
+          group of users.
+        </Translate>
       </>
     ),
   },
   {
-    title: <>Simple architecture</>,
+    title: (
+      <>
+        <Translate
+          id='indexPage.features.label2.title'
+          description='features label2 title'>
+          Simple architecture
+        </Translate>
+      </>
+    ),
     imageUrl: 'img/pages/home/feature-house.svg',
     description: (
       <>
-        No more complex architecture with JavaScript frontend, REST API backend, database, cache, etc.
-        With Flet you just write a monolith stateful app in Python only and get multi-user, realtime Single-Page Application (SPA).
+        <Translate
+          id='indexPage.features.label2.content'
+          description='features label2 content'>
+          No more complex architecture with JavaScript frontend, REST API
+          backend, database, cache, etc. With Flet you just write a monolith
+          stateful app in Python only and get multi-user, realtime Single-Page
+          Application (SPA).
+        </Translate>
       </>
     ),
   },
   {
-    title: <>Batteries included</>,
+    title: (
+      <>
+        <Translate
+          id='indexPage.features.label3.title'
+          description='features label3 title'>
+          Batteries included
+        </Translate>
+      </>
+    ),
     imageUrl: 'img/pages/home/feature-battery.svg',
     description: (
       <>
-        To start developing with Flet, you just need your favorite IDE or text editor.
-        No SDKs, no thousands of dependencies, no complex tooling - Flet has built-in web server with assets hosting and desktop clients.
+        <Translate
+          id='indexPage.features.label3.content'
+          description='features label3 content'>
+          To start developing with Flet, you just need your favorite IDE or text
+          editor. No SDKs, no thousands of dependencies, no complex tooling -
+          Flet has built-in web server with assets hosting and desktop clients.
+        </Translate>
       </>
     ),
   },
   {
-    title: <>Powered by Flutter</>,
+    title: (
+      <>
+        <Translate
+          id='indexPage.features.label4.title'
+          description='features label4 title'>
+          Powered by Flutter
+        </Translate>
+      </>
+    ),
     imageUrl: 'img/pages/home/feature-flutter.svg',
     description: (
       <>
-        Flet UI is built with <a href="https://flutter.dev">Flutter</a>, so your app looks professional and can be delivered to any platform.
-        Flet simplifies Flutter model by combining smaller "widgets" into ready-to-use "controls" with imperative programming model.
+        <Translate
+          id='indexPage.features.label4.content1'
+          description='features label4 content1'>
+          Flet UI is built with
+        </Translate>{' '}
+        <a href='https://flutter.dev'>Flutter</a>
+        <Translate
+          id='indexPage.features.label4.content2'
+          description='features label4 content2'>
+          , so your app looks professional and can be delivered to any platform.
+          Flet simplifies Flutter model by combining smaller "widgets" into
+          ready-to-use "controls" with imperative programming model.
+        </Translate>
       </>
     ),
   },
   {
-    title: <>Speaks your language</>,
+    title: (
+      <>
+        <Translate
+          id='indexPage.features.label5.title'
+          description='features label5 title'>
+          Speaks your language
+        </Translate>
+      </>
+    ),
     imageUrl: 'img/pages/home/feature-language.svg',
     description: (
       <>
-        Flet is language-agnostic, so anyone on your team could develop Flet apps in their favorite language. <a href="/docs/guides/python/getting-started">Python</a> is already supported, Go, C# and others are <a href="/roadmap">coming next</a>.
+        <Translate
+          id='indexPage.features.label5.content1'
+          description='features label5 content1'>
+          Flet is language-agnostic, so anyone on your team could develop Flet
+          apps in their favorite language.
+        </Translate>{' '}
+        <a href='/docs/guides/python/getting-started'>Python</a>{' '}
+        <Translate
+          id='indexPage.features.label5.content2'
+          description='features label5 content2'>
+          is already supported, Go, C# and others are
+        </Translate>
+        <a href='/roadmap'>
+          {' '}
+          <Translate
+            id='indexPage.features.label5.content3'
+            description='features label5 content3'>
+            coming next.
+          </Translate>
+        </a>
       </>
     ),
   },
   {
-    title: <>Deliver to any device</>,
+    title: (
+      <>
+        <Translate
+          id='indexPage.features.label6.title'
+          description='features label6 title'>
+          Deliver to any device
+        </Translate>
+      </>
+    ),
     imageUrl: 'img/pages/home/feature-mobile.svg',
     description: (
       <>
-        Deploy Flet app as a web app and view it in a browser. Package it as a standalone desktop app for Windows, macOS and Linux.
-        Install it on mobile as <a href="https://web.dev/what-are-pwas/">PWA</a> or view via Flet app for iOS and Android.
+        <Translate
+          id='indexPage.features.label6.content1'
+          description='features label6 content1'>
+          Deploy Flet app as a web app and view it in a browser. Package it as a
+          standalone desktop app for Windows, macOS and Linux. Install it on
+          mobile as
+        </Translate>{' '}
+        <a href='https://web.dev/what-are-pwas/'>PWA</a>{' '}
+        <Translate
+          id='indexPage.features.label6.content2'
+          description='features label6 content2'>
+          or view via Flet app for iOS and Android.
+        </Translate>
       </>
     ),
   },
-];
+]
 
 function Feature({ imageUrl, title, description }) {
-  const imgUrl = useBaseUrl(imageUrl);
+  const imgUrl = useBaseUrl(imageUrl)
   return (
     <div className={clsx('col col--4', styles.feature)}>
       {imgUrl && (
-        <div className="text--center">
+        <div className='text--center'>
           <img className={styles.featureImage} src={imgUrl} alt={title} />
         </div>
       )}
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
-  );
+  )
 }
 
 function Home() {
-  const context = useDocusaurusContext();
-  const { siteConfig = {} } = context;
+  const context = useDocusaurusContext()
+  const { siteConfig = {} } = context
   return (
     <Layout
-      title={`${siteConfig.customFields.heroTitle}`}
-      description={`${siteConfig.tagline}`}>
+      title={translate({
+        id: 'indexPage.title.heroTitle',
+        message: 'The fastest way to build Flutter apps in Python',
+        description: 'the title heroTitle description',
+      })}
+      description={translate({
+        id: 'indexPage.title.description',
+        message:
+          'Build internal web apps quickly in the language you already know.',
+        description: 'the description heroSubTitle description',
+      })}>
       <main>
-        <div className="container margin-bottom--lg">
+        <div className='container margin-bottom--lg'>
           <div className={clsx('flet-hero', styles.heroBanner)}>
-            <div className="row">
-              <div className="col  col--6">
+            <div className='row'>
+              <div className='col  col--6'>
                 {/* <h2>It's amazing how little code you need to get amazing results!</h2> */}
-                <img src="img/pages/home/flet-home.png" style={{ width: '100%' }}></img>
+                <img
+                  src='img/pages/home/flet-home.png'
+                  style={{ width: '100%' }}></img>
               </div>
-              <div className="col col--6">
-                <h1 className="hero__title">{siteConfig.customFields.heroTitle}</h1>
-                <p className="hero__subtitle">{siteConfig.customFields.heroSubTitle}</p>
+              <div className='col col--6'>
+                <h1 className='hero__title'>
+                  <Translate
+                    id='indexPage.heroTitle'
+                    description='the heroTitle description'>
+                    The fastest way to build Flutter apps in Python
+                  </Translate>
+                </h1>
+                <p className='hero__subtitle'>
+                  <Translate
+                    id='indexPage.heroSubTitle'
+                    description='the heroSubTitle description'>
+                    Flet enables developers to easily build realtime web, mobile
+                    and desktop apps in Python. No frontend experience required.
+                  </Translate>
+                </p>
                 <div className={styles.buttons}>
                   <Link
                     className={styles.indexCtasGetStartedButton}
                     to={useBaseUrl('docs/')}>
-                    Get Started
+                    <Translate
+                      id='indexPage.button.getStarted'
+                      description='the getStarted button description'>
+                      Get Started
+                    </Translate>
                   </Link>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="container text--center">
-          <h2>Main features</h2>
+        <div className='container text--center'>
+          <h2>
+            <Translate
+              id='indexPage.features.title'
+              description='the features title description'>
+              Main features
+            </Translate>
+          </h2>
           {features && features.length > 0 && (
             <section className={styles.features}>
-              <div className="container">
-                <div className="row">
+              <div className='container'>
+                <div className='row'>
                   {features.map((props, idx) => (
                     <Feature key={idx} {...props} />
                   ))}
@@ -133,7 +274,7 @@ function Home() {
         <SignupForm />
       </main>
     </Layout>
-  );
+  )
 }
 
-export default Home;
+export default Home
