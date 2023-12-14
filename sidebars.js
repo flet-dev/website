14350255f0d198384dfc2997770419e06334f916
ndonkoHenri <robotcoder4@protonmail.com>
2023-12-14 05:13:49 +0100

    CupertinoRadio

--- sidebars.js
+++ sidebars.js
@@ -178,6 +178,7 @@ module.exports = {
             "controls/checkbox",
             "controls/chip",
             "controls/cupertinocheckbox",
+            "controls/cupertinoradio",
             "controls/cupertinoswitch",
             "controls/dropdown",
             "controls/radio",
